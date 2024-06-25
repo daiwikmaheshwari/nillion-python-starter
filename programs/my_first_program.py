fe3c1b64e7f5adbdbf00c91eb7019dea6c5f8376
daiwikmaheshwari <daiwikmahesh@gmail.com>
2024-06-25 18:33:07 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -2,11 +2,16 @@ from nada_dsl import *
 
 def nada_main():
     party1 = Party(name="Party1")
-    my_int1 = SecretInteger(Input(name="my_int1", party=party1))
-    my_int2 = SecretInteger(Input(name="my_int2", party=party1))
+    # Start date of the event
+    HackerHouse = SecretInteger(Input(name="HackerHouse", party=party1, value=200))
+    Rocks = SecretInteger(Input(name="Rocks", party=party1, value=208))
 
-    # Perform the addition of my_int1 and my_int2
-    result = my_int1 + my_int2
+    # Computation for sum and product of HackerHouse and Rocks
+    sum_result = HackerHouse + Rocks
+    product_result = HackerHouse * Rocks
 
-    # Return the result of the addition
-    return [Output(result, "my_output", party1)]
+    # Outputs the sum and product to party1 with new labels
+    return [
+        Output(sum_result, "HackerHouse", party1),
+        Output(product_result, "WillRock", party1)
+    ]
